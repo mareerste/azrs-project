9e7811554bc870b4a64e8ab1796858c74a042b29
2Dan3 <dan23.ftn@gmail.com>
2022-04-19 17:06:42 +0200

    Add graceful-shutdown

--- main.go
+++ main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+// GRACEFUL SHUTDOWN :
 
 func main() {
-	fmt.Println("Hello World")
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+
+	router := mux.NewRouter()
+	router.StrictSlash(true)
+
+	server := Service{
+		data: map[string][]*Config{},
+	}
+	router.HandleFunc("/config/", server.createConfigHandler).Methods("POST") // PRAVI KONF. ILI KONF. GRUPU
+	router.HandleFunc("/config/", server.getAllConfig).Methods("GET")
+	router.HandleFunc("/config/{id}/", server.getConfigHandler).Methods("GET")
+	router.HandleFunc("/config/{id}/", server.delConfigHandler).Methods("DELETE")
+
+	// start server
+	srv := &http.Server{Addr: "0.0.0.0:8000", Handler: router}
+	go func() {
+		log.Println("server starting")
+		if err := srv.ListenAndServe(); err != nil {
+			if err != http.ErrServerClosed {
+				log.Fatal(err)
+			}
+		}
+	}()
+
+	<-quit
+
+	log.Println("service shutting down ...")
+
+	// gracefully stop server
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("server stopped")
 }
